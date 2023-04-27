2cf87c3a640cb819c0cc3aec63903ac433fa555b
ali-iqbal-brainx <ali.iqbal@brainxtech.com>
2023-04-28 03:26:45 +0500

    add stamp for messages

--- models/private.js
+++ models/private.js
@@ -33,6 +33,14 @@ const privateSchema = new Schema({
             message: {
                 type: String,
                 required: true,
+            },
+            createdAt: {
+                type: Date,
+                default: Date.now
+            },
+            updatedAt: {
+                type: Date,
+                default: Date.now
             }
         }
     ],
--- models/public.js
+++ models/public.js
@@ -23,6 +23,14 @@ const publicSchema = new Schema({
             message: {
                 type: String,
                 required: true,
+            },
+            createdAt: {
+                type: Date,
+                default: Date.now
+            },
+            updatedAt: {
+                type: Date,
+                default: Date.now
             }
         }
     ],
